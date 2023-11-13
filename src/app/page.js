6c859d9a6fc5bdc39c15f149107fc1c526c3a161
mazpeng <mazgpeng@gmail.com>
2023-11-13 08:15:27 +0700

    rest api

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,23 @@
 import AnimeList from "@/components/AnimeList";
-import Link from "next/link";
+import Header from "@/components/AnimeList/header";
 
 const Home = async () => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
   );
-  const anime = await response.json();
-  console.log(anime);
+  const topAnime = await response.json();
+  console.log(topAnime);
 
   return (
     <>
-      <div className="flex justify-between p-4">
-        <h1 className="text-2xl">Paling Populer</h1>
-        <Link
-          href="/populer"
-          className="text-xl underline hover:text-indigo-500 transition-all"
-        >
-          Lihat Semua
-        </Link>
-      </div>
-
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-5">
-        {anime.data.map((data) => {
-          return (
-            <div key={data.mal_id} className="shadow-xl">
-              <AnimeList
-                title={data.title}
-                images={data.images.webp.image_url}
-                id={data.mal_id}
-              />
-            </div>
-          );
-        })}
-      </div>
+      <section>
+        <Header
+          title="Paling Populer"
+          linkTitle="Lihat Semua"
+          linkHref="/populer"
+        />
+        <AnimeList api={topAnime} />
+      </section>
     </>
   );
 };
