712e8cc3ae8b8f51ee31cd2ef615d562dd07f3f1
mazpeng <mazgpeng@gmail.com>
2023-11-15 22:57:53 +0700

    Warning: Each child in a list should have a unique key prop.

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -3,11 +3,12 @@ import Link from "next/link";
 const AnimeList = ({ api }) => {
   return (
     <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 px-40">
-      {api.data.map((anime) => {
+      {api.data.map((anime, index) => {
         return (
           <Link
             href={`/${anime.mal_id}`}
             className="cursor-pointer hover:text-accent text-primary transition-all"
+            key={index}
           >
             <Image
               src={anime.images.webp.image_url}
