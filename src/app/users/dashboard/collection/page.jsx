bae23cd797fcecdbe52a4680897f67709bf76350
mazpeng <mazgpeng@gmail.com>
2023-11-24 23:56:18 +0700

    deploy

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Header from "@/components/Dashboard/header";
+import Header from "@/components/Dashboard/Header";
 
 const Page = () => {
   return (
